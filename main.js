6aac2cef4dc35555e7b4f8c5a278120870d7151c
Tautminas Cibulskis <tautminas@yahoo.com>
2024-02-16 23:45:45 +0200

    Add resizability

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import "./style.css";
 
 // Scene
 const scene = new THREE.Scene();
@@ -9,18 +10,46 @@ const material = new THREE.MeshStandardMaterial({ color: 0x00ff83 });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
+// Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
 // Light
 const light = new THREE.PointLight(0xffffff, 70, 100, 1.7);
 light.position.set(0, 10, 10);
 scene.add(light);
 
 // Camera
-const camera = new THREE.PerspectiveCamera(45, 800 / 600, 0.1, 100);
+const camera = new THREE.PerspectiveCamera(
+  45,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
 camera.position.z = 20;
 scene.add(camera);
 
 // Renderer
 const canvas = document.querySelector(".webgl");
-const render = new THREE.WebGLRenderer({ canvas });
-render.setSize(800, 600);
-render.render(scene, camera);
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
+
+window.addEventListener("resize", () => {
+  // Update Sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update Camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(sizes.width, sizes.height);
+});
+
+const loop = () => {
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(loop);
+};
+loop();
